9608e04fc4f61610a079136352abdffa927f5207
hongdoan1 <doanhongg199@gmail.com>
2023-11-24 03:43:08 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/hngsapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.hngsapplication.app
+
+import com.hngsapplication.app.appcomponents.base.BaseActivity
+import com.hngsapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
